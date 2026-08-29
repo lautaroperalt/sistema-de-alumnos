bb48d595f13fe9157a60c9749aab67551f76b729
Lautaro Peralta <pperalta.lautaro@gmail.com>
2026-08-29 14:36:52 -0300

    La primera clase

---
+++ Sistema de Alumnos/Alumno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sistema_de_Alumnos
+{
+    public class Alumno
+    {
+        public required string name { get; set; }
+        public int file { get; set; }
+        public decimal note1 { get; set; }
+        public decimal note2 { get; set; }
+    }
+}
---
+++ Sistema de Alumnos/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using Sistema_de_Alumnos;
+
+Alumno alumnoUno = new Alumno()
+{
+    name = "Lautaro"
+};
+alumnoUno.note1 = 9.99m;
+alumnoUno.note2 = 9.10m;
+alumnoUno.file = 2;
+
+Console.WriteLine($"El nombre del alumno es {alumnoUno.name} y cuenta con un legajo de {alumnoUno.file} paginas");
+
+Alumno alumnoDos = new Alumno()
+{
+    name = "Tomas"
+};
+alumnoDos.note1 = 7.11m;
+alumnoDos.note2 = 7m;
+alumnoDos.file = 2;
+
+Console.WriteLine($"El nombre del alumno es {alumnoDos.name} y cuenta con un legajo de {alumnoDos.file} paginas");
